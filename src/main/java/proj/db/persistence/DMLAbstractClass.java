d1a6e80f7232091e26e31e4d178d9b0cb2765bb7
Joey <joeycarlofrancisco@gmail.com>
2021-09-02 00:00:29 +0800

    Adjusted format

--- src/main/java/proj/db/persistence/DMLAbstractClass.java
+++ src/main/java/proj/db/persistence/DMLAbstractClass.java
@@ -1,5 +1,6 @@
 package proj.db.persistence;
 
+import java.lang.reflect.InvocationTargetException;
 import java.sql.Connection;
 import java.sql.Date;
 import java.sql.DriverManager;
@@ -9,81 +10,82 @@ import java.sql.SQLException;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import proj.db.persistence.datasource.DBSourceFactory;
 
 public abstract class DMLAbstractClass<T> {
 
-    private static final Logger logger = LoggerFactory.getLogger(DMLAbstractClass.class);
-    private Connection connection = null;
-    private PreparedStatement prepareStmt = null;
-    private static final String oracleConnectionString = "jdbc:oracle:thin:@localhost:1521:orcl";
-    private static final String username = "HR";
-    private static final String password = "xl2411Z$";
+  private static final Logger logger = LoggerFactory.getLogger(DMLAbstractClass.class);
+  private Connection connection = null;
+  private PreparedStatement prepareStmt = null;
 
-    protected Connection getConnection() throws SQLException {
-        logger.debug("===================Attempting to get connection===================");
-        connection = DriverManager.getConnection(oracleConnectionString, username, password);
-        logger.debug("===================Connection obtained!===================");
-        return connection;
+  protected Connection getConnection()
+      throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException, InvocationTargetException, NoSuchMethodException {
+    logger.debug("===================Attempting to get connection===================");
+    connection = DBSourceFactory.getConnection(DBSourceFactory.MAIN);
+    logger.debug("===================Connection obtained!===================");
+    return connection;
+  }
+
+  protected void prepareStatement(String sql) throws SQLException {
+    if (connection == null) {
+      logger.error("No connection initialized! call getConnection() to get connection");
+      throw new SQLException("Connection instance is null!");
     }
+    prepareStmt = connection.prepareStatement(sql);
+  }
 
-    protected void prepareStatement(String sql) throws SQLException {
-        if (connection == null) {
-            logger.error("No connection initialized! call getConnection() to get connection");
-            throw new SQLException("Connection instance is null!");
-        }
-        prepareStmt = connection.prepareStatement(sql);
+  protected void setString(int parameter, String value) throws SQLException {
+    prepareStmt.setString(parameter, value);
+  }
+
+  protected void setInt(int parameter, int value) throws SQLException {
+    prepareStmt.setInt(parameter, value);
+  }
+
+  protected void setDouble(int parameter, double value) throws SQLException {
+    prepareStmt.setDouble(parameter, value);
+  }
+
+  protected void setByte(int parameter, byte value) throws SQLException {
+    prepareStmt.setByte(parameter, value);
+  }
+
+  protected void setBytes(int parameter, byte[] value) throws SQLException {
+    prepareStmt.setBytes(parameter, value);
+  }
+
+  protected void setDate(int parameter, Date value) throws SQLException {
+    prepareStmt.setDate(parameter, value);
+  }
+
+  protected int executeUpdate() throws SQLException {
+    return prepareStmt.executeUpdate();
+  }
+
+  protected ResultSet executeQuery() throws SQLException {
+    return prepareStmt.executeQuery();
+  }
+
+  protected void closeResources() throws SQLException {
+    logger.debug("Closing resources..");
+    if (connection != null || connection.isClosed()) {
+      connection.close();
+      connection = null;
     }
-
-    protected void setString(int parameter, String value) throws SQLException {
-        prepareStmt.setString(parameter, value);
+    if (prepareStmt != null || prepareStmt.isClosed()) {
+      prepareStmt.close();
+      prepareStmt = null;
     }
+  }
 
-    protected void setInt(int parameter, int value) throws SQLException {
-        prepareStmt.setInt(parameter, value);
-    }
+  public abstract T selectData(T searchCriteria);
 
-    protected void setDouble(int parameter, double value) throws SQLException {
-        prepareStmt.setDouble(parameter, value);
-    }
+  public abstract T[] selectBulk(T searchCriteria);
 
-    protected void setByte(int parameter, byte value) throws SQLException {
-        prepareStmt.setByte(parameter, value);
-    }
+  public abstract int updateData(T data);
 
-    protected void setBytes(int parameter, byte[] value) throws SQLException {
-        prepareStmt.setBytes(parameter, value);
-    }
+  public abstract int deleteData(T data);
 
-    protected void setDate(int parameter, Date value) throws SQLException {
-        prepareStmt.setDate(parameter, value);
-    }
-
-    protected int executeUpdate() throws SQLException {
-        return prepareStmt.executeUpdate();
-    }
-
-    protected ResultSet executeQuery() throws SQLException {
-        return prepareStmt.executeQuery();
-    }
-
-    protected void closeResources() throws SQLException {
-        logger.debug("Closing resources..");
-        if (connection != null || connection.isClosed()) {
-            connection.close();
-            connection = null;
-        }
-        if (prepareStmt != null || prepareStmt.isClosed()) {
-            prepareStmt.close();
-            prepareStmt = null;
-        }
-    }
-
-    public abstract T selectData(T searchCriteria);
-
-    public abstract T[] selectBulk(T searchCriteria);
-
-    public abstract int updateData(T data);
-
-    public abstract int deleteData(T data);
+  public abstract T readSingleResult(ResultSet resultSet);
 
 }
